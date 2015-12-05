d8129317a32630496aaa301232b588f3ca311fd1
Geoffrey Mon <geofbot@gmail.com>
2015-12-05 12:17:31 -0500

    Move around logic

--- client.py
+++ client.py
@@ -24,9 +24,14 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.bind((host, port))
 tcp_socket.listen(5)
 
+permmode = ''
+if len(sys.argv) > 1:
+    print 'tcp only client mode'
+    permmode = 'tcp'
+
 while True:
-    mode = ""
-    data = ""
+    mode = ''
+    data = ''
     
     try:        
         sock, addr = tcp_socket.accept()
@@ -34,22 +39,22 @@ while True:
         print data
         if data:
             response = ''
-            if len(sys.argv) < 1:
+            if permmode == 'tcp':
+                mode = 'tcp'
+            else:
                 mode = "udp"
                 udp_connector.send(data)
-                #udp_connector.send('\n\n')
                 ready = select.select([udp_receiver], [], [], tcp_socket.getdefaulttimeout())
                 if ready[0]:
                     response = udp_receiver.recv(4096 * 32)
                     print response
-            else:
-                mode = "tcp"
             if mode == "tcp" or not ready[0] or response.startswith('USETCP'): # use tcp
+                if permmode == 'tcp':
+                    print 'not using UDP...'
                 print "switching to TCP"
                 tcp_connector = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 tcp_connector.connect((remote, remotePort))
                 tcp_connector.send(data)
-                #tcp_connector.send('\n\n')
                 response = tcp_connector.recv(4096 * 32)
 
         print response
